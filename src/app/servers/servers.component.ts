8dd35d4f7d3544a46497e27b5ddbf8199d19aae9
kjwhydonate <kausthub@whydonate.com>
2023-06-21 20:16:07 +0530

    Test property binding - servers

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+}
