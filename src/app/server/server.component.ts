ad4aa5731de8cd9efaebb0a180cc8c9597420a97
kjwhydonate <kausthub@whydonate.com>
2023-06-22 18:24:16 +0530

    add dynamic styling with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html',
 })
 export class ServerComponent {
-  serverId: number = 10;
-  serverStatus: string = 'online';
+  serverId: number = Math.round(Math.random() * 10);
+  serverStatus: string = Math.random() > 0.5 ? 'online' : 'offline';
+
+  getColor() {
+    return this.serverStatus == 'online' ? 'green' : 'red';
+  }
 }
