4891024cc37ec3cf34ebbf9459453b9333cf21d9
kjwhydonate <kausthub@whydonate.com>
2023-06-26 19:29:54 +0530

    Binding to custom elements cockpit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   serverElements = [
     { type: 'server', name: 'Test Server', content: 'Just a test' },
   ];
+
+  onServerAdd(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdd(bluePrintData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent,
+    });
+  }
 }
