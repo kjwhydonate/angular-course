f37f5c143aafa2f3c1d647c78fc08172946a5fb1
kjwhydonate <kausthub@whydonate.com>
2023-06-26 18:27:09 +0530

    refactored code of app.component.html

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
 })
 export class ServerElementComponent {
-
+  element: { type: string; name: string; content: string };
 }
