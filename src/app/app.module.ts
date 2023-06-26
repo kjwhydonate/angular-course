1fdcf8df659cb823e378a6dc59bc75a8d7762e74
kjwhydonate <kausthub@whydonate.com>
2023-06-26 17:59:18 +0530

    create new dir for module 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+// Module 3
+// import { HeaderComponent } from './module-3/header/header.component';
+// import { RecipesComponent } from './module-3/recipes/recipes.component';
+// import { RecipeDetailComponent } from './module-3/recipes/recipe-detail/recipe-detail.component';
+// import { RecipeListComponent } from './module-3/recipes/recipe-list/recipe-list.component';
+// import { RecipeItemComponent } from './module-3/recipes/recipe-list/recipe-item/recipe-item.component';
+// import { ShoppingListComponent } from './module-3/shopping-list/shopping-list.component';
+// import { ShoppingEditComponent } from './module-3/shopping-list/shopping-edit/shopping-edit.component';
 
 // Module 1 and 2
 // import { ServerComponent } from './module-1-and-2/server/server.component';
@@ -20,7 +22,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 // import { Assignment3Component } from './assignment3/assignment3.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeDetailComponent, RecipeListComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
