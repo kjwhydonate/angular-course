823040f42d359f19dbfe065436c0c200e50dd6b2
kjwhydonate <kausthub@whydonate.com>
2023-06-30 14:42:16 +0530

    Assignment 4

--- src/app/even/even.component.ts
+++ src/app/even/even.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-even',
   templateUrl: './even.component.html',
   styleUrls: ['./even.component.css']
 })
-export class EvenComponent {
+export class EvenComponent implements OnInit {
+  @Input() number: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
